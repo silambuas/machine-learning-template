9c3b733dcd5ea29d88f2bdd22ac1aef11475d4fa
silambuas <flytoasmano@gamil.com>
2018-07-14 20:04:53 +0530

    First commit

---
+++ demp.py
@@ -0,0 +1,34 @@
+#importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y =dataset.iloc[:, 3].values
+
+#missing_data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis =0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features= [0])
+x = onehotencoder.fit_transform(x).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into training and test dataset
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.4,random_state = 0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
